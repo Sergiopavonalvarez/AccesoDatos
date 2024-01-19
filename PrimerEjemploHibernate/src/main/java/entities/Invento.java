c6b9ea6a34bcd5dd4f2d1882baa3a21f39643f61
Sergio_Pavon <pavon_madrid999@hotmail.com>
2024-01-19 21:08:41 +0100

    21:08 19/01

--- PrimerEjemploHibernate/src/main/java/entities/Invento.java
+++ PrimerEjemploHibernate/src/main/java/entities/Invento.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table (name = "inventos")
+@Table(name = "inventos")
 
 public class Invento {
 	@Id
@@ -14,32 +14,34 @@ public class Invento {
 	@Column
 	private String inventor;
 	private int fecha;
+
 	public String getNombeInvento() {
 		return nombeInvento;
 	}
+
 	public void setNombeInvento(String nombeInvento) {
 		this.nombeInvento = nombeInvento;
 	}
+
 	public String getInventor() {
 		return inventor;
 	}
+
 	public void setInventor(String inventor) {
 		this.inventor = inventor;
 	}
+
 	public int getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(int fecha) {
 		this.fecha = fecha;
 	}
+
 	@Override
 	public String toString() {
 		return "Invento [nombeInvento=" + nombeInvento + ", inventor=" + inventor + ", fecha=" + fecha + "]";
 	}
-	
-	
-	
-	
-	
 
 }
