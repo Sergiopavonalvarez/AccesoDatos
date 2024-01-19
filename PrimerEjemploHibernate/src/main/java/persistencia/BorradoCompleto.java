3454474835a6b5243364a094a39136f5d100576a
Sergio_Pavon <pavon_madrid999@hotmail.com>
2024-01-19 18:44:35 +0100

    18:44 19/01

--- PrimerEjemploHibernate/src/main/java/persistencia/BorradoCompleto.java
+++ PrimerEjemploHibernate/src/main/java/persistencia/BorradoCompleto.java
@@ -31,7 +31,6 @@ public class BorradoCompleto {
 		}
 
 		session.getTransaction().commit();
-		;
 		session.close();
 		sf.close();
 
--- PrimerEjemploHibernate/src/main/java/persistencia/TestMain.java
+++ PrimerEjemploHibernate/src/main/java/persistencia/TestMain.java
@@ -17,18 +17,23 @@ public class TestMain {
 			session = sf.openSession();
 			Invento test1 = new Invento();
 			Invento test2 = new Invento();
+			Invento test3 = new Invento();
 			test1.setFecha(1454);
 			test1.setInventor("Gutemberg");
 			test1.setNombeInvento("Imprenta");
-		
 
 			test2.setFecha(1769);
 			test2.setInventor("Edward Jenner");
 			test2.setNombeInvento("Vacuna Viruela");
 
+			test3.setFecha(1895);
+			test3.setInventor("Lumiere");
+			test3.setNombeInvento("Cinematografo");
+
 			session.beginTransaction();
 			session.persist(test1);
 			session.persist(test2);
+			session.persist(test3);
 			session.getTransaction().commit();
 
 		} catch (Exception e) {
